fc555e5c265bdfb475d9c6521f76e11874edcace
feyza yarcı <yarcifeyza@gmail.com>
2025-01-27 06:44:59 +0300

    edited

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,48 +1,145 @@
 import './js/init';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
-
-
-const fetchPostsBtn = document.querySelector('.btn');
-const postsList = document.querySelector('.posts');
-const apiKey = '48254147-b4b10b5266c157a9a3946e15d';
-const url = `https://pixabay.com/api/?key=${apiKey}&${searchParams.toString()}`;
 
 let page = 1;
 let perPage = 40;
+let loadedPosts = [];
+let currentSearchTerm = '';
+const searchForm = document.querySelector('#searchForm');
+const galleryDiv = document.querySelector('.gallery');
+const loadingSpinnerDiv = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('#loadMoreBtn');
 
-fetchPostsBtn.addEventListener('click', async () => {
+
+searchForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
+  const search = document.querySelector('#search').value.trim();
+
+  if (search !== currentSearchTerm) {
+    currentSearchTerm = search;
+    page = 1;
+  }
+  loadingSpinnerDiv.style.display = 'block';
+
   try {
-    const posts = await fetchPostsBtn();
-    renderPosts(posts);
-    page += 1;
-    if (page > 1) {
-      fetchPostsBtn.textContent = 'LOAD MORE';
+    const posts = await fetchPosts(search);
+
+    if (posts.length <= 0) {
+      iziToast.error({
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight'
+      });
+      loadMoreBtn.classList.add('hidden');
+    } else {
+      loadedPosts = posts;
+      renderPosts(posts);
+      page += 1;
+      loadMoreBtn.classList.remove('hidden');
     }
+
+    document.querySelector('#search').value = '';
+
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    loadMoreBtn.classList.add('hidden');
+  } finally {
+    loadingSpinnerDiv.style.display = 'none';
   }
 });
-async function fetchPosts() {
-  const params = new URLSearchParams(
-    {
-      _limit: perPage,
-      _page: page,
-    }
-  );
+
+
+async function fetchPosts(search) {
+  const apiKey = '48254147-b4b10b5266c157a9a3946e15d';
+  const searchParams = new URLSearchParams({
+    q: search,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: perPage,
+    page: page
+  });
+  const url = `https://pixabay.com/api/?key=${apiKey}&${searchParams.toString()}`;
   const response = await axios.get(url);
-  return response.data;
-}
-function renderPosts(posts) {
-  const markup = posts.map(({ id, tiitle, body, userId }) => {
-    return `<li>
-    <h2 class="post-title">${tiitle.slice(0, 30)}</h2>
-    <p><b>Post Id</b>: ${id}</p>
-    <p><b>Author Id</b>: ${userId}</p>
-    <p>${body}</p>
-    </li>
-    `;
-  })
-    .join("");
-  postsList.insertAdjacentHTML('beforeend', markup);
+  const totalHits = response.data.totalHits;
+  const totalPages = Math.ceil(totalHits / perPage);
+
+  if (page >= totalPages) {
+    iziToast.info({
+      message: "We're sorry, but you've reached the end of search results",
+      position: 'topCenter'
+    });
+    loadMoreBtn.classList.add('hidden');
+  }
+  return response.data.hits;
 }
 
+function renderPosts(images, append = false) {
+  const galleryContent = images
+    .map(image => {
+      return `
+        <a href="${image.largeImageURL}" class="image-card">
+          <img src="${image.webformatURL}" alt="${image.tags}" />
+          <div class="image-info">
+            <div class="likes">
+              <p class="likes-text">Likes</p>
+              <p class="likes-count">${image.likes}</p>
+            </div>
+            <div class="views">
+              <p class="views-text">Views</p>
+              <p class="views-count">${image.views}</p>
+            </div>
+            <div class="comments">
+              <p class="comments-text">Comments</p>
+              <p class="comments-count">${image.comments}</p>
+            </div>
+            <div class="downloads">
+              <p class="downloads-text">Downloads</p>
+              <p class="downloads-count">${image.downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    }).join('');
+
+  if (append) {
+    galleryDiv.innerHTML += galleryContent;
+  } else {
+    galleryDiv.innerHTML = galleryContent;
+  }
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+  });
+  lightbox.refresh();
+}
+
+loadMoreBtn.addEventListener('click', async function (event) {
+  event.preventDefault();
+  loadingSpinnerDiv.style.display = 'block';
+
+  try {
+    const posts = await fetchPosts(currentSearchTerm);
+    renderPosts(posts, true);
+    page += 1;
+
+    const galleryItems = document.querySelectorAll('.image-card');
+    if (galleryItems.length > 0) {
+      const firstItemHeight = galleryItems[0].getBoundingClientRect().height;
+
+      window.scrollBy({
+        top: firstItemHeight * 2.0,
+        behavior: 'smooth'
+      });
+    }
+
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    loadingSpinnerDiv.style.display = 'none';
+  }
+});
